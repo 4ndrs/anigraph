33e0f4e1e2febff0a3ae5117854dfd2f85a6780d
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-05-06 04:04:34 +0200

    Add provisional alias to headers & update .gitignore

--- src/anigraph.py
+++ src/anigraph.py
@@ -9,11 +9,11 @@
 # SPDX-License-Identifier: MIT
 #
 from sys import argv
-import headers
+import headers as ani
 
 # Load the config file
 conf = dict()
-config(conf)
+ani.config(conf)
 
 # If there is no config file, assume it's the first run
-if len(conf) < 1: first_run(conf)
+if len(conf) < 1: ani.first_run(conf)
