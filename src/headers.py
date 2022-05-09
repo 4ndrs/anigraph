905204dbe6ead41b2decc6cb571cc2a65f274de0
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-05-09 06:32:55 +0200

    Implement sync function & add minor changes
    The synching works, but the server returns entries with the same IDs
    will need to change the schema again, I can't seem to find the
    differences on these duplicates

--- src/headers.py
+++ src/headers.py
@@ -8,6 +8,7 @@
 # SPDX-License-Identifier: MIT
 #
 import os
+import time
 import sqlite3
 import queries
 import requests
@@ -113,17 +114,16 @@ def print_help():
 
 # Sync the database with upstream
 def sync_db(conf_path):
-    print('Initializing sync...')
+    print('Processing sync...')
     con = sqlite3.connect(os.path.join(conf_path, _db_name))
     cur = con.cursor()
 
-    # Get user name from the database
-    cur.execute(queries.get_user_name)
-
-    _page       = 1 # Current page
-    _perpage    = 5 # Items per page
-    _username   = cur.fetchone()[0]
-    _userdb     = _username + '.db' # Database for user's list
+    _chunk   	    = 1 # Current chunk
+    _sync_perchunk  = 10 # Items per chunk for normal sync
+    _full_perchunk  = 100 # Items per chunk for first  sync
+    _username       = cur.execute(queries.get_user_name).fetchone()[0]
+    _token          = cur.execute(queries.get_user_token).fetchone()[0]
+    _userdb         = _username + '.db' # Database for user's list
 
     con.close()
     con = sqlite3.connect(os.path.join(conf_path, _userdb))
@@ -134,14 +134,104 @@ def sync_db(conf_path):
     cur.executescript(queries.userdb_initialize)
     con.commit()
 
-    variables = {
-        'page'      : _page,
-        'perPage'   : _perpage,
-        'userName'  : _username
-    }
+    _db_last_updated = cur.execute(queries.userdb_get_last_updated).fetchone()[0]
+    if _db_last_updated is None: _perchunk = _full_perchunk # For big syncs, only the first time when no DB
+    else:                        _perchunk = _sync_perchunk # Checking small changes little by little
 
+    if _perchunk == _full_perchunk: print('No local data found. Trying a full sync.', end='', flush=True)
+    else: print('Trying a normal sync.', end='', flush=True)
+
+    count = 0
+    while True:
+        variables = {
+            'Chunk'     : _chunk,
+            'perChunk'  : _perchunk,
+            'userName'  : _username
+        }
+
+        request = requests.post(_api_url, json={'query': queries.get_user_list, 'variables': variables},
+                                headers = {'Authorization': 'Bearer ' + _token})
+        _upstream_last_updated =\
+        request.json()['data']['MediaListCollection']['lists'][0]['entries'][0]['media']['mediaListEntry']['updatedAt']
+        _has_nextchunk = request.json()['data']['MediaListCollection']['hasNextChunk']
+        _rate_limit_remaining = request.headers['X-RateLimit-Remaining']
+        _status_code = request.status_code
+
+        if int(_rate_limit_remaining) < 5: time.sleep(60) # Avoid going over the rate limit
+
+        if _status_code != 200:
+            print('Unhandled status code:', _status_code)
+            break
+
+        # Check if there has been any changes upstream before sync
+        if _perchunk == _sync_perchunk and _db_last_updated == _upstream_last_updated:
+            print('\nLocal data is up to date with upstream')
+            break
+
+        # Now we can sync, or pull the full list from upstream
+        for listx in request.json()['data']['MediaListCollection']['lists']:
+            for entry in listx['entries']:
+                entry = entry['media']
+                _id             = entry['id']
+                _episodes       = entry['episodes']
+                _title_romaji   = entry['title']['romaji']
+                _title_native   = entry['title']['native']
+                _season         = entry['season']
+                _season_year    = entry['seasonYear']
+                _genres         = entry['genres']
+                _score          = entry['mediaListEntry']['score']
+                _status         = entry['mediaListEntry']['status']
+                _progress       = entry['mediaListEntry']['progress']
+                _updated_at     = entry['mediaListEntry']['updatedAt']
+
+                # For debugging purposes
+                #print('id:', _id)
+                #print('episodes:', _episodes)
+                #print('title:', _title_romaji)
+                #print('title:', _title_native)
+                #print('season:', _season)
+                #print('season:', _season_year)
+                #print('genres:', _genres)
+                #print('score:', _score)
+                #print('status:', _status)
+                #print('progress:', _progress)
+                #print('updated:', _updated_at)
+                    #print('Entry number: %i\nID: %i\nUpdated at: %i\nTitle: %s\n日本語: %s\n'\
+                #      'Season: %s %i\nScore: %i\nStatus: %s\nProgress: %s\nEpisodes: %s\n'\
+                #      % (count, _id, _updated_at, _title_romaji, _title_native, _season, _season_year,
+                #         _score, _status, _progress, _episodes))
+
+                if _season is None:
+                    _season      = 'null_season'
+                    _season_year = 0
+
+                # Update the database
+                cur.execute(queries.userdb_insert_status,  (_status,))
+                cur.execute(queries.userdb_insert_seasons, (_season, _season_year))
+                for _genre in _genres: cur.execute(queries.userdb_insert_genres, (_genre,))
+
+                # Let's pull this entry's status_id, and season_id
+                _season_id = cur.execute(queries.userdb_get_season_id, (_season, _season_year)).fetchone()[0]
+                _status_id = cur.execute(queries.userdb_get_status_id, (_status,)).fetchone()[0]
+
+                cur.execute(queries.userdb_insert_series, (_id, _title_romaji, _title_native,
+                                                           _episodes, _progress, _score,
+                                                           _updated_at, _season_id, _status_id))
+                # Series and Genres connections
+                for _genre in _genres:
+                    _genre_id = cur.execute(queries.userdb_get_genre_id, (_genre,)).fetchone()[0]
+                    cur.execute(queries.userdb_insert_series_and_genres, (_id, _genre_id))
+
+                count += 1
+
+        # HDD too slow, moved commit here
+        con.commit()
+        print('.', end='', flush=True)
+        if not _has_nextchunk: break
+        else:  _chunk += 1
+        time.sleep(1)
     con.close()
-    pass
+    print('\nProcessed', count, 'entries')
 
 # Extract the data from the local database and generate the js/html files
 def export_stats(conf_path, save_path):
--- src/queries.py
+++ src/queries.py
@@ -19,7 +19,39 @@ get_user =\
   }
 }
 '''
+
 get_user_list =\
+'''
+query ($userName: String, $perChunk: Int, $Chunk: Int) {
+  MediaListCollection(userName: $userName, type:ANIME, perChunk: $perChunk,
+					  chunk:$Chunk, sort: UPDATED_TIME_DESC){
+    hasNextChunk
+    lists {
+      entries {
+        media {
+          id
+		  episodes
+          title {
+            romaji
+            native
+          }
+          season
+          seasonYear
+          genres
+          mediaListEntry {
+            updatedAt
+            score (format: POINT_100)
+			status
+			progress
+          }
+        }
+      }
+    }
+  }
+}
+'''
+
+_get_user_list =\
 '''
 {
   Page(page: $page, perPage:$perPage) {
@@ -55,16 +87,74 @@ CREATE TABLE IF NOT EXISTS auth_user (
     token TEXT
 )
 '''
+
 save_user_auth =\
 '''
 INSERT INTO auth_user (id, name, token) VALUES (?, ?, ?)
 '''
+
 get_user_name =\
 '''
 SELECT name FROM auth_user
 '''
+
+get_user_token =\
+'''
+SELECT token FROM auth_user
+'''
+
+userdb_get_last_updated =\
+'''
+SELECT MAX(updated_at) FROM Series
+'''
+
+userdb_get_season_id =\
+'''
+SELECT id FROM Seasons WHERE season = ? AND season_year = ?
+'''
+
+userdb_get_status_id =\
+'''
+SELECT id FROM Status WHERE status = ?
+'''
+
+userdb_get_genre_id =\
+'''
+SELECT id FROM Genres WHERE genre = ?
+'''
+
+userdb_insert_status =\
+'''
+INSERT OR IGNORE INTO Status (status) VALUES (?)
+'''
+
+userdb_insert_seasons =\
+'''
+INSERT OR IGNORE INTO Seasons (season, season_year) VALUES (?, ?)
+'''
+
+userdb_insert_genres =\
+'''
+INSERT OR IGNORE INTO Genres (genre) VALUES (?)
+'''
+
+userdb_insert_series =\
+'''
+INSERT OR REPLACE INTO Series (id, title_romaji, title_native, episodes, progress, score, updated_at,
+                               season_id, status_id) VALUES (?,?,?,?,?,?,?,?,?)
+'''
+
+userdb_insert_series_and_genres =\
+'''
+INSERT OR IGNORE INTO Series_and_Genres (series_id, genre_id) VALUES (?, ?)
+'''
+
 userdb_initialize =\
 '''
+CREATE TABLE IF NOT EXISTS Status (
+	id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	status          TEXT UNIQUE
+);
 CREATE TABLE IF NOT EXISTS Seasons (
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
     season          TEXT,
@@ -81,8 +171,12 @@ CREATE TABLE IF NOT EXISTS Series (
     id INTEGER PRIMARY KEY UNIQUE,
     title_romaji    TEXT,
     title_native    TEXT,
-    score REAL,
-    season_id INTEGER
+	episodes        INTEGER,
+	progress        INTEGER,
+    score           INTEGER,
+    updated_at      INTEGER,
+    season_id       INTEGER,
+    status_id       INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Characters (
